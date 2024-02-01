4adfe64ae3d53e769705b099291f0ad3ffe30f5a
Tammon23 <Tammon2000@gmail.com>
2024-02-01 18:50:30 -0500

    Added Utility function -  Function: SpawnInvisibleArmorstand spawns an NMS armorstand that is invisible, optionally small, and optionally no clipped

--- src/main/java/me/tammon/minecraftsmashheroes/Helper.java
+++ src/main/java/me/tammon/minecraftsmashheroes/Helper.java
@@ -1,7 +1,13 @@
 package me.tammon.minecraftsmashheroes;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.decoration.ArmorStand;
 import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_20_R3.CraftWorld;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
@@ -13,5 +19,23 @@ public class Helper {
         item.setItemMeta(item_meta);
 
         return item;
+    }
+
+    public static Entity SpawnInvisibleArmorStand(Location location, boolean make_small, boolean noclip){
+        ServerLevel w = ((CraftWorld)location.getWorld()).getHandle();
+        ArmorStand armorStand = new ArmorStand(w, location.getX(), location.getY(), location.getZ());
+        armorStand.setXRot(location.getYaw());
+        armorStand.setYRot(location.getPitch());
+        armorStand.setInvisible(true);
+
+        if (make_small)
+            armorStand.setSmall(true);
+
+        if (noclip)
+            armorStand.noPhysics = true;
+
+        w.addFreshEntity(armorStand, CreatureSpawnEvent.SpawnReason.CUSTOM);
+
+        return armorStand.getBukkitEntity();
     }
 }
