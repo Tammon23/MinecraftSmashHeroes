2eb39dba707d020ca74db963408870ffc29f2f5a
Tammon23 <Tammon2000@gmail.com>
2024-02-01 19:03:07 -0500

    Removed unneeded listeners

--- src/main/java/me/tammon/minecraftsmashheroes/Heroes/CustomHeroes/Tinman/TinmanShooter.java
+++ src/main/java/me/tammon/minecraftsmashheroes/Heroes/CustomHeroes/Tinman/TinmanShooter.java
@@ -3,14 +3,13 @@ package me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.Tinman;
 import me.tammon.minecraftsmashheroes.Features.BaseAbilities.DelayedRanged;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TinmanShooter extends DelayedRanged implements Listener {
+public class TinmanShooter extends DelayedRanged{
     private static ItemStack tinman_shooter;
     private static int max_bullet_life;
     private static int speed;
