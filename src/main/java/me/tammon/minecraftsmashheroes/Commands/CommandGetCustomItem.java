b3cbe9425dd3483e9b589f0d32cc53d4f38ae75e
Tammon23 <Tammon2000@gmail.com>
2024-02-02 23:57:57 -0500

    Added a smash crystal powerup - with customizable pick and deletion actions

--- src/main/java/me/tammon/minecraftsmashheroes/Commands/CommandGetCustomItem.java
+++ src/main/java/me/tammon/minecraftsmashheroes/Commands/CommandGetCustomItem.java
@@ -1,11 +1,13 @@
 package me.tammon.minecraftsmashheroes.Commands;
 
-import me.tammon.minecraftsmashheroes.Events.Karakot.KarakotHandUse;
+import me.tammon.minecraftsmashheroes.Features.Powerups.SmashCrystal;
 import me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.GeneralCluck.Blaster;
 import me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.Karakot.Kamehameha;
 import me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.Shoop.ShoopShooter;
 import me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.Skullfire.SkullfireGun;
 import me.tammon.minecraftsmashheroes.Heroes.CustomHeroes.Tinman.TinmanShooter;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -46,6 +48,24 @@ public class CommandGetCustomItem implements CommandExecutor {
 
             case "general_cluck_blaster":
                 player_inventory.addItem(Blaster.get_general_cluck_blaster());
+                break;
+
+            case "smash_crystal":
+                float x = Float.parseFloat(args[1]);
+                float y = Float.parseFloat(args[2]);
+                float z = Float.parseFloat(args[3]);
+                long countdown_timer = Long.parseLong(args[4]);
+
+                SmashCrystal smashCrystal = new SmashCrystal(
+                        player.getWorld(),
+                        new Location(player.getWorld(), x, y, z),
+                        Material.PINK_STAINED_GLASS_PANE.createBlockData(),
+                        Material.RED_CARPET.createBlockData(),
+                        countdown_timer
+                );
+
+                smashCrystal.start();
+
                 break;
 
             default:
---
+++ src/main/java/me/tammon/minecraftsmashheroes/Features/Powerups/SmashCrystal.java
@@ -0,0 +1,98 @@
+package me.tammon.minecraftsmashheroes.Features.Powerups;
+
+import me.tammon.minecraftsmashheroes.MinecraftSmashHeroes;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Transformation;
+
+import java.util.Collection;
+import java.util.function.Predicate;
+
+public class SmashCrystal extends BukkitRunnable {
+    private final World world;
+    private final Location spawn_location;
+    private final BlockData ready_state;
+    private final BlockDisplay smash_crystal;
+    private final long countdown_timer;
+    private final int id;
+
+    private Predicate<Collection<Entity>> on_collided = (entity -> true);
+
+
+    public SmashCrystal(World world, Location spawn_location, BlockData loading_state, BlockData ready_state, long countdown_timer){
+        this.world = world;
+        this.spawn_location = spawn_location;
+        this.ready_state = ready_state;
+        this.countdown_timer = countdown_timer;
+
+        BlockDisplay blockDisplay = (BlockDisplay) world.spawnEntity(
+                spawn_location,
+                EntityType.BLOCK_DISPLAY
+        );
+
+        Transformation transformation = blockDisplay.getTransformation();
+        transformation.getTranslation().x = -0.5f;
+
+        blockDisplay.setTransformation(transformation);
+        blockDisplay.setBlock(loading_state);
+
+        this.smash_crystal = blockDisplay;
+        this.id = blockDisplay.getEntityId();
+    }
+
+    public void setTransformation(Transformation transformation){
+        this.smash_crystal.setTransformation(transformation);
+    }
+
+    public void setOn_collided(Predicate<Collection<Entity>> on_collided){
+        this.on_collided = on_collided;
+    }
+
+    public void start(){
+
+        new BukkitRunnable() {
+            int i = 0;
+
+            @Override
+            public void run(){
+                if (i == 361){
+                    i = 0;
+                }
+                smash_crystal.setRotation(i, 0);
+                i += 4;
+            }
+        }.runTaskTimer(MinecraftSmashHeroes.PLUGIN, 0, 1);
+
+        // change block data of current block display to ready state
+        // start runnable to detect when it gets collided with
+        // limit check to every 5 ticks
+        Bukkit.getScheduler().runTaskLater(MinecraftSmashHeroes.PLUGIN, () -> {
+            this.smash_crystal.setBlock(this.ready_state);
+            super.runTaskTimer(MinecraftSmashHeroes.PLUGIN, 0, 5);
+        }, this.countdown_timer);
+
+    }
+
+
+    @Override
+    public void run() {
+        // if we collide with an entity, relay it
+        Collection<Entity> nearbyEntities = world.getNearbyEntities(
+                this.spawn_location,
+                1, 1, 1,
+                entity -> entity.getEntityId() != this.id);
+
+        if (!nearbyEntities.isEmpty())
+            if (this.on_collided.test(nearbyEntities)){
+                this.smash_crystal.remove();
+                this.cancel();
+            }
+
+    }
+}
