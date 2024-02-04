b2b3642076b04d4852fbe3143a9b579bdf348db8
Tammon23 <Tammon2000@gmail.com>
2024-02-03 20:55:42 -0500

    Updated Dash - renamed quickDash to smooth - Added 2 new dashes quick and forward - Added command options for testing new ones

--- src/main/java/me/tammon/minecraftsmashheroes/Commands/CommandDash.java
+++ src/main/java/me/tammon/minecraftsmashheroes/Commands/CommandDash.java
@@ -21,12 +21,34 @@ public class CommandDash implements CommandExecutor {
 
         Player player = (Player) sender;
         switch (args[0].toLowerCase()){
-            case "quickdash":
+            case "smooth":
                 if (args.length == 1)
-                    Dash.quickDash(player);
+                    Dash.smooth(player);
                 else
-                    Dash.quickDash(player, Float.parseFloat(args[1]));
+                    Dash.smooth(player, Float.parseFloat(args[1]));
                 break;
+
+            case "quick":
+                if (args.length == 2)
+                    Dash.quick(player, Long.parseLong(args[1]));
+
+                else if (args.length > 2)
+                    Dash.quick(player, Float.parseFloat(args[1]), Long.parseLong(args[2]));
+
+                else
+                    player.sendMessage("Improper usage not enough arguments");
+
+                break;
+
+            case "forward":
+                if (args.length == 2)
+                    Dash.forward(player, Long.parseLong(args[1]));
+
+                else if (args.length > 2)
+                    Dash.forward(player, Float.parseFloat(args[1]), Long.parseLong(args[2]));
+
+                else
+                    player.sendMessage("Improper usage not enough arguments");
 
             default:
                 player.sendMessage("Improper usage, unhandled option: " + args[0]);
--- src/main/java/me/tammon/minecraftsmashheroes/Features/Dash.java
+++ src/main/java/me/tammon/minecraftsmashheroes/Features/Dash.java
@@ -1,15 +1,59 @@
 package me.tammon.minecraftsmashheroes.Features;
 
+import me.tammon.minecraftsmashheroes.MinecraftSmashHeroes;
 import org.bukkit.entity.Entity;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class Dash {
 
-    public static void quickDash(Entity entity){
-        quickDash(entity, 1);
+    public static void smooth(Entity entity){
+        smooth(entity, 1);
     }
-    public static void quickDash(Entity entity, float dashFactor){
+    public static void smooth(Entity entity, float dashFactor){
         Vector unitDirection = entity.getLocation().getDirection().normalize();
         entity.setVelocity(unitDirection.multiply(dashFactor));
+    }
+
+    public static void quick(Entity entity, long maxTicks){
+        quick(entity, 1, maxTicks);
+    }
+
+    public static void quick(Entity entity, float dashFactor, long maxTicks){
+        Vector unitDirection = entity.getLocation().getDirection().normalize().multiply(dashFactor);
+        new BukkitRunnable() {
+            long ticksLeft = maxTicks;
+            @Override
+            public void run() {
+                if (ticksLeft <= 0){
+                    entity.setVelocity(new Vector());
+                    this.cancel();
+                }
+
+                entity.setVelocity(unitDirection);
+                ticksLeft--;
+            }
+        }.runTaskTimer(MinecraftSmashHeroes.PLUGIN,0L, 1L);
+    }
+
+    public static void forward(Entity entity, long maxTicks){
+        forward(entity, 1, maxTicks);
+    }
+    public static void forward(Entity entity, float dashFactor, long maxTicks){
+        new BukkitRunnable() {
+            long ticksLeft = maxTicks;
+            @Override
+            public void run() {
+                Vector unitDirection = entity.getLocation().getDirection().normalize().multiply(dashFactor);
+
+                if (ticksLeft <= 0){
+                    entity.setVelocity(new Vector());
+                    this.cancel();
+                }
+
+                entity.setVelocity(unitDirection);
+                ticksLeft--;
+            }
+        }.runTaskTimer(MinecraftSmashHeroes.PLUGIN,0L, 1L);
     }
 }
